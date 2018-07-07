90dd0ae2495a541d2ecd5896bc8ce1bb3e8c5777
Stuart Black <stuartblack@gmail.com>
2018-07-07 11:19:46 +0100

    Party checks and fill room to capacity functions completed

--- room.rb
+++ room.rb
@@ -1,75 +1,117 @@
 #require_relative('customer')
 class Room
 
-  attr_reader :name, :entry_fee, :guests, :songs, :holding_bay
-  attr_accessor :money
-def initialize(name, entry_fee, songs, room_size)
-  @name = name
-  @entry_fee = entry_fee
-  @guests = []
-  @songs = [@song1, @song2, @song3]
-  @room_size = room_size
-  @holding_bay = []
-end
+  attr_reader :name, :entry_fee, :guests, :songs, :lobby
+  attr_accessor :money, :guests
+  def initialize(name, entry_fee, songs, room_size)
+    @name = name
+    @entry_fee = entry_fee
+    @guests = []
+    @songs = [@song1, @song2, @song3]
+    @room_size = room_size
+    @lobby = []
+  end
 
+  ##############################################
 
-
-##############################################
-
-def check_in_guest(guest)
-  puts puts
-  p "_____________CHECK IN GUEST________"
-  p "There Were #{@guests.length} guests..."
+  def check_in_guest(guest)
+    puts puts
+    p "_____________CHECK IN GUEST________"
+    p "There Were #{@guests.length} guests..."
     @guests << guest
-  p "Theres now #{@guests.length} guests..."
-end
+    p "Theres now #{@guests.length} guests..."
+  end
 
-def check_out_guest(guests)
-  puts puts
-  p "_____________CHECK OUT GUEST________"
-  p "There Were #{@guests.length} guests..."
-  @guests.shift
-  p "Theres now #{@guests.length} guests..."
-end
+  def check_out_guest(guests)
+    puts puts
+    p "_____________CHECK OUT GUEST________"
+    p "There Were #{@guests.length} guests..."
+    @guests.shift
+    p "Theres now #{@guests.length} guests..."
+  end
 
-def check_in_song(song)
-  puts puts
-  p "________CHECK IN SONG________"
-  p "Song #{song.song_name} was added"
-  p "There Were #{@songs.length} songs..."
-  @songs << song
-  p "Theres now #{@songs.length} songs..."
-end
+  def check_in_song(song)
+    puts puts
+    p "________CHECK IN SONG________"
+    p "Song #{song.song_name} was added"
+    p "There Were #{@songs.length} songs..."
+    @songs << song
+    p "Theres now #{@songs.length} songs..."
+  end
 
-def entry_fee(client)
-  puts puts
-  p "____CUSTOMER CHARGED ENTRY FEE________"
-  p "Customers money before entry_fee is 100?"
-  p client.money
-  client.money -= @entry_fee #attr_acc: money
-  p "Customers wallet after entry_fee is 80?"
-  p client.money
-end
+  def entry_fee(client)
+    puts puts
+    p "____CUSTOMER CHARGED ENTRY FEE________"
+    p "Customers money before entry_fee is 100?"
+    p client.money
+    client.money -= @entry_fee #attr_acc: money
+    p "Customers wallet after entry_fee is 80?"
+    p client.money
+  end
 
-def entry_fee_check(client2)
-  puts puts
-  p "____IS CUSTOMER 2 SKINT________"
-  p "Customers money before entry_fee is 15?"
-  p client2.money
-if client2.money >= @entry_fee
-  client2.money -= @entry_fee #attr_acc: money
-else
-  p "Customers Denied Entry. Needs #{@entry_fee - client2.money} more money"
-  p client2.money
-end
-end
+  def entry_fee_check(client2)
+    puts puts
+    p "____IS CUSTOMER 2 SKINT________"
+    p "Customers money before entry_fee is 15?"
+    p client2.money
+    if client2.money >= @entry_fee
+      client2.money -= @entry_fee #attr_acc: money
+    else
+      p "Customers Denied Entry. Needs #{@entry_fee - client2.money} more money"
+      p client2.money
+      @lobby << client2
+      p "Customer is back in the lobby"
+      p @lobby.length
+    end
+  end
+
+  def room_party_too_big(lobby)
+    puts puts
+    p "______PARTY TOO BIG_______"
+    while lobby.length <= @room_size
+      @guests << lobby
+    end
+    p "Sorry you're party is too big. #{lobby.length} doesn't go into #{@room_size}, come back later"
+  end
+
+  def room_fill_to_capacity(lobby)
+    puts puts
+    p "_______ROOM CAPACITY________"
+    p "Room Size is *#{@room_size}* and there are *#{lobby.length}* guests"
+    lobby.each do |guest|
+      while guests.length < @room_size
+        @guests << guest
+        lobby.pop
+        p "Guest #{guests.length} added "
+      end
+    end
+    puts "______Room now at MAX Capacity Sorry______"
+    p "Guests Inside: #{guests.length} Room Capacity: #{@room_size} Turned away : #{lobby.length}"
+    lobby.each do |name| turned_away = name.name
+      p "#{turned_away} Can't go in, go home #{turned_away}"
+    end
+
+    # --------------------------------------------------------------
+    #   p "______ROOM CAPACITY_______"
+    #   p "Guest size #{@guests.length}  ---"
+    #   p  "Space available = #{@room_size}"
+    #   while guests.length < @room_size
+    #   @guests << @guest1
+    #   p "Guest size #{@guests.length}  ---"
+    # end
+    #   p guests.each {|guest| p guest }
+    #   p "Room now at MAX Capacity #{guests.length} / #{@room_size}"
+    # end
+  end
+
+  # def room_has_guest_fav_song()
+  # p "ROOM HAS GUEST FAV SONG?"
+  #
+  # end
 
 
 
 end # class end
-
-
-
 
 # test = Empty.new("Harry", 50)
 #
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,15 +8,20 @@ class RoomTest < MiniTest::Test
 
 def setup
                       #name   entry_fee  array   room_size
-  @room1 = Room.new("Barrowlands", 20, @songs, 2)
+  @room1 = Room.new("Barrowlands", 20, @songs, 3)
+  @room2 = Room.new("Albert Hall", 200, @songs, 2)
                     #name   money favourite_song
   @client1 = Guest.new("Ted", 100, "Hurricane")
   @client2 = Guest.new("Kevin", 15, "Brass In Pocket")
+  @client3 = Guest.new("Joanna", 100, "No Surprises")
+  @client4 = Guest.new("Joanna", 100, "No Surprises")
+  @client5 = Guest.new("Joanna", 100, "No Surprises")
                     #artist      song_name    lyrics
   @song1 = Song.new("Bob Dylan", "Hurricane", "waaaaa")
   @song2 = Song.new("Kevin Morby", "Aboard My Train", "Heee")
   @song3 = Song.new("Radiohead", "No Surprises", "plingplong")
   @song4 = Song.new("The Pretenders", "Brass In Pocket", "ah oh ohh ohhhhoohh")
+  @lobby = [@client1, @client2,@client3, @client4, @client5]
 end
 
 
@@ -51,5 +56,24 @@ def test_entry_fee_check
   @room1.entry_fee_check(@client2)
   assert_equal(15, @client2.money)
 end
+
+def test_room_party_too_big
+  @room2.room_party_too_big(@lobby)
+  assert_equal(nil, @length)
+end
+
+def test_room_fill_to_capacity
+  @room1.room_fill_to_capacity(@lobby)
+  assert_equal(3, @room1.guests.length)
+end
+# def test_room_has_guest_fav_song
+#   @room1.room_party_too_big(@lobby)  # put party in the room
+#   p room_has_guest_fav_song
+# end
+
+
+
+
+
 
 end # class end
